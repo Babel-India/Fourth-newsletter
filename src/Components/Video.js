5dc5f0ee8aeae0b01727fdeb487e2b6fa67fb4c4
Babel-India <84664679+Babel-India@users.noreply.github.com>
2021-10-20 10:36:53 +0530

    Update Video.js

--- src/Components/Video.js
+++ src/Components/Video.js
@@ -6,7 +6,7 @@ import video1 from '../Images/Video/Tongue_Twister.mov';
 function Video(){
     return(
         <div id="experts">
-            <h1 className='video-head'> Experts Corner</h1>
+            <h1 className='video-head'> Just For Fun </h1>
             <Container>
                     <video className = "vimmy"  src={video1} controls='controls' width='35%' height='10%' autoPlay='false' />
             </Container>
